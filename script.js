58a5e01721bf3c1ad9be29b7941f66481db2d69a
codesparks <muniuelsie@gmail.com>
2025-03-01 21:28:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Get menu button and list
-const menuToggle = document.querySelector(".mobile-nav-toggle");
-const navLinks = document.querySelector(".nav-links");
-    navLinks.classList.toggle("active");
-    
+// Display current date
+document.addEventListener("DOMContentLoaded", function () {
+    let dateElement = document.getElementById("currentDate");
+    if (dateElement) {
+        let today = new Date();
+        let options = { year: "numeric", month: "long", day: "numeric" };
+        dateElement.innerText = today.toLocaleDateString("en-US", options);
+    }
+});
